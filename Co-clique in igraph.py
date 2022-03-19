f68bdc39ea76dbd5b57fdd5440fbdf10dd4d438d
thepriceless <89837375+thepriceless@users.noreply.github.com>
2022-03-19 21:36:31 +0300

    Create Co-clique in igraph.py

---
+++ Co-clique in igraph.py
@@ -0,0 +1,17 @@
+import igraph as ig
+from igraph import *
+
+added = set()
+g = Graph()
+g.add_vertices(52)
+
+with open('edges-list.txt') as f:
+    for i in range(194):
+        a = f.readline().split()
+        temp = tuple((a[0], a[1]))
+        if temp not in added:
+            g.add_edge(int(a[0]) - 1, int(a[1]) - 1)
+            temp = tuple((a[1], a[0]))
+            added.add(temp)
+print(g.independence_number())
+print(g.largest_independent_vertex_sets())
